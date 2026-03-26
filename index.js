16702d58a22af30f5a0ecac37fc4c1b9eaa2bfc4
Félix Pietruczanis <felix.pietruczanis@proton.me>
2026-03-26 23:43:30 +0100

    uibuild

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
 const express = require("express")
 const morgan = require('morgan')
+const Person = require('./models/person')
+
 const app = express()
 
 app.use(express.static('dist'))
@@ -16,36 +19,9 @@ app.use(morgan(function (tokens, req, res) {
   ].join(' ')
 }))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get("/info", (request, response) => {
     response.send(`Phonebook has info for ${persons.length} people <br /> ${new Date().toString()}`)
 })
-
-const generateId = () => {
-    return String(Math.floor(Math.random() * 1000))
-}
 
 app.post("/api/persons", (request, response) => {
     const body = request.body
@@ -62,29 +38,26 @@ app.post("/api/persons", (request, response) => {
         })
     }
 
-    const person = {
-        id: generateId(),
+    const person = new Person({
         name: body.name,
         number: body.number
-    }
+    })
+    
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
+})
 
-    persons = persons.concat(person)
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(notes => {
+    response.json(notes)
+  })
+})
 
+app.get('/api/persons/:id', (request, response) => {
+  Note.findById(request.params.id).then(person => {
     response.json(person)
-})
-
-app.get("/api/persons", (request, response) => {
-    response.json(persons)
-})
-
-app.get("/api/persons/:id", (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-    if (person) {
-       response.json(person)
-    } else {
-        response.status(404).end()
-    }
+  })
 })
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -94,7 +67,7 @@ app.delete("/api/persons/:id", (request, response) => {
     response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
